68c43ad3f118825a99a9abb94aa0229daad5f23d
Patrick Hao <patrick.hao95@gmail.com>
2019-07-25 18:40:22 +1000

    Everything works.

--- andrew.py
+++ andrew.py
@@ -1,38 +1,110 @@
-
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
 
 from keras import optimizers
 from keras import layers, Sequential
 from keras.applications.densenet import preprocess_input, DenseNet121
 from keras.utils.np_utils import to_categorical
-
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import h5py
+from keras.callbacks import Callback
+from sklearn.metrics import cohen_kappa_score
+
+IMG_SIZE = 384  # this must correspond with what is in .h5 file
+NUM_CLASSES = 5  # 5 output classes
+NUM_EPOCHS = 100  # number of epochs
+BATCH_SIZE = 6
+
+class Metrics(Callback):
+
+    def on_epoch_end(self, epoch, logs={}):
+        X_val, y_val = self.validation_data[:2]
+        y_val = y_val.sum(axis=1) - 1
+
+        y_pred = self.model.predict(X_val) > 0.5
+        y_pred = y_pred.astype(int).sum(axis=1) - 1
+
+        _val_kappa = cohen_kappa_score(
+            y_val,
+            y_pred,
+            weights='quadratic'
+        )
+
+        self.val_kappas.append(_val_kappa)
+
+        print("val_kappa: {}".format(_val_kappa))
+
+        # if _val_kappa == max(self.val_kappas):
+        # print("Validation Kappa has improved. Saving model.")
+        # self.model.save('model.h5')
+
+        return
 
 
 def get_values():
-    file = h5py.File('/tmp/{}'.format('data_rgb_384_new_processed.h5'), 'r')
-    x_train, y_train, x_test, y_test = file['x_train'], file['y_train'], file['x_test'], file['y_test']
+    file = h5py.File('./data/data_rgb_384_processed.h5', 'r')
+    return file['x_train'], file['y_train'], file['x_test'], file['y_test']
+
+
+def generate_imgs_from_file(path, batch_size):
+    while True:
+        file = h5py.File(path, 'r')
+        xy_train = list(zip(file['x_train'], file['y_train']))
+        len_xy_train = len(xy_train)
+        batch_start = 0
+        batch_end = batch_size
+        while batch_start < len_xy_train:
+            limit = min(batch_end, len_xy_train)
+            x_train = [xy[0] for xy in xy_train[batch_start:limit]]
+            y_train = [to_categorical(xy[1], NUM_CLASSES) for xy in xy_train[batch_start:limit]]
+            yield(np.array(x_train), np.array(y_train))
+            batch_start += batch_size
+            batch_end += batch_size
+        file.close()
+
+        # for tpl in xy_train:
+        #     yield(np.asarray([tpl[0]]), np.asarray([tpl[1]]))
+        # file.close()
+
 
 def main():
 
 
+    x_train, y_train, x_test, y_test = get_values()
 
+    # x_train = (n for n in x_train)
+    # x_test = (n for n in x_test)
+    # gc.collect()
 
-    IMG_SIZE = 384  # this must correspond with what is in .h5 file
-    NUM_CLASSES = 5  # 5 output classes
-    NUM_EPOCHS = 100  # number of epochs
-    BATCH_SIZE = 32
 
     y_train = to_categorical(y_train, NUM_CLASSES)
     y_test = to_categorical(y_test, NUM_CLASSES)
+
+    kappa_metrics = Metrics()
+    callbacks_list = []
+
+    datagen = ImageDataGenerator(
+        horizontal_flip=True,
+        vertical_flip=True,
+        rotation_range=360
+    )
+
+    datagenVal = ImageDataGenerator()
+
     model = Sequential()
-    model.add(densenet)
 
     densenet = DenseNet121(
-        weights='./DenseNet-BC-121-32-no-top',
+        weights='./DenseNet-BC-121-32-no-top.h5',
         include_top=False,
         input_shape=(IMG_SIZE, IMG_SIZE, 3)
     )
+
+    model.add(densenet)
+
+
 
     model.add(layers.GlobalAveragePooling2D())
     # model.add(layers.Flatten())
@@ -49,12 +121,17 @@ def main():
 
     # fits the model on batches with real-time data augmentation:
     history = model.fit_generator(
-        datagen.flow(x_train, y_train, batch_size=12),
-        steps_per_epoch=len(x_train) / 12,
+        # generate_imgs_from_file('./data/data_rgb_384_processed.h5', BATCH_SIZE),
+        datagen.flow(x_train, y_train, batch_size=BATCH_SIZE),
+        steps_per_epoch=len(x_train) // BATCH_SIZE,
         epochs=NUM_EPOCHS,
-        validation_data=(x_test, y_test),
+        # validation_data=(x_test, y_test),
+        validation_data=datagenVal.flow(x_test, y_test, batch_size=BATCH_SIZE),
+        validation_steps=len(x_test) // BATCH_SIZE,
         callbacks=callbacks_list,
-        max_queue_size=2
+        # use_multiprocessing=True,
+        # workers=2
+        # max_queue_size=2
     )
 
 
