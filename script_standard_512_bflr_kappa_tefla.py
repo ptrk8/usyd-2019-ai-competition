a2ec95b308868cadd0ce004127b811e28427bab8
Andrew Ray Lee <andrewl@maths.usyd.edu.au>
2019-07-30 02:12:54 +1000

    Pushing from server.

--- script_standard_512_bflr_kappa_tefla.py
+++ script_standard_512_bflr_kappa_tefla.py
@@ -1,3 +1,6 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -5,23 +8,21 @@ session = tf.Session(config=config)
 
 from keras import optimizers
 from keras import layers, Sequential
-# from keras.applications.densenet import preprocess_input, DenseNet121
-from keras.applications.vgg16 import VGG16, preprocess_input
+from keras.applications.densenet import preprocess_input, DenseNet121
 from keras.utils.np_utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import h5py
 from keras.callbacks import Callback, LearningRateScheduler
 from sklearn.metrics import cohen_kappa_score
-from utils import get_custom_callback, to_multi_label, cohen_kappa_loss, f1_m, best_lr_decay, kappa_loss, f1_loss, multi_label_acc
-import os
+from utils import get_custom_callback, to_multi_label, best_lr_decay, f1_m, multi_label_acc, kappa_loss_tefla
 import sys
 
 
-IMG_SIZE = 384  # this must correspond with what is in .h5 file
+IMG_SIZE = 512  # this must correspond with what is in .h5 file
 NUM_CLASSES = 5  # 5 output classes
 NUM_EPOCHS = 50  # number of epochs
-BATCH_SIZE = 10
+BATCH_SIZE = 8
 
 
 def main():
@@ -41,12 +42,8 @@ def main():
     custom_callback = get_custom_callback('multi_label', './{}'.format(output_path_name))
     callbacks_list = [custom_callback, learn_rate]
 
-    file = h5py.File('/albona/nobackup/andrewl/DeepLearning/data/data_rgb_384_new.h5', 'r')
+    file = h5py.File('/albona/nobackup/andrewl/DeepLearning/data/data_rgb_512_processed.h5', 'r')
     x_train, y_train, x_test, y_test = file['x_train'], file['y_train'], file['x_test'], file['y_test']
-
-    # x_train, x_test = preprocess_input(x_train), preprocess_input(x_test)
-    x_train = x_train
-    y_train = y_train
 
     y_train = to_categorical(y_train, NUM_CLASSES)
     y_test = to_categorical(y_test, NUM_CLASSES)
@@ -55,14 +52,14 @@ def main():
     y_test = to_multi_label(y_test)
 
     datagen = ImageDataGenerator(
-        # horizontal_flip=True,
-        # vertical_flip=True,
-        # rotation_range=360
+        horizontal_flip=True,
+        vertical_flip=True,
+        rotation_range=360
     )
 
     model = Sequential()
 
-    densenet = VGG16(
+    densenet = DenseNet121(
         weights='imagenet',
         include_top=False,
         input_shape=(IMG_SIZE, IMG_SIZE, 3)
@@ -72,21 +69,15 @@ def main():
     model.add(layers.GlobalAveragePooling2D())
     model.add(layers.Dropout(0.5))
     # model.add(layers.Dense(NUM_CLASSES, activation='softmax'))
-    model.add(layers.Dense(5, activation='sigmoid'))
+    model.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
 
     model.summary()
-    model_cohen_kappa = cohen_kappa_loss(num_classes=5)
 
-    # model_cohen_kappa = get_cohen_kappa()
-    model.compile(
-        # loss=kappa_loss,
-	          loss=f1_loss,
-                  # loss='binary_crossentropy',
-                  optimizer=optimizers.Adam(),
-                  # optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
-                  metrics=[multi_label_acc, f1_m])
+    model.compile(loss=kappa_loss_tefla,
+                  # optimizer=optimizers.Adam(lr=0.0001,decay=1e-6),
+                  optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
+                  metrics=[f1_m, multi_label_acc])
 
-    # return
     # fits the model on batches with real-time data augmentation:
     history = model.fit_generator(
         datagen.flow(x_train, y_train, batch_size=BATCH_SIZE, seed=1),
