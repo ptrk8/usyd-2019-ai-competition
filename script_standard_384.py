3e39e349ffffca07f6503708e40e3fa9628bc5f6
Andrew Ray Lee <andrewl@maths.usyd.edu.au>
2019-07-29 20:18:01 +1000

    Made changes to submission in order to make it work on server.

---
+++ script_standard_384.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+from keras import optimizers
+from keras import layers, Sequential
+from keras.applications.densenet import preprocess_input, DenseNet121
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import h5py
+from keras.callbacks import Callback
+from sklearn.metrics import cohen_kappa_score
+from utils import get_custom_callback, to_multi_label
+import os
+import sys
+
+
+IMG_SIZE = 384  # this must correspond with what is in .h5 file
+NUM_CLASSES = 5  # 5 output classes
+NUM_EPOCHS = 50  # number of epochs
+BATCH_SIZE = 12
+
+
+def main():
+    # Name of this script
+    script_name = os.path.basename(__file__)[0:-3]
+    # Construct folder name using name of this script
+    output_path_name = '_{}_outputs'.format(script_name)
+    # Try to create a new folder
+    try:
+        # Make the output folder
+        os.mkdir(output_path_name)
+    except FileExistsError:
+        pass
+
+    # Model below this line ================================================
+
+    custom_callback = get_custom_callback('multi_label', './{}'.format(output_path_name))
+    callbacks_list = [custom_callback]
+
+    file = h5py.File('/albona/nobackup/andrewl/DeepLearning/data/data_rgb_384_new_processed.h5', 'r')
+    x_train, y_train, x_test, y_test = file['x_train'], file['y_train'], file['x_test'], file['y_test']
+
+    y_train = to_categorical(y_train, NUM_CLASSES)
+    y_test = to_categorical(y_test, NUM_CLASSES)
+
+    y_train = to_multi_label(y_train)
+    y_test = to_multi_label(y_test)
+
+    datagen = ImageDataGenerator(
+        horizontal_flip=True,
+        vertical_flip=True,
+        rotation_range=360
+    )
+
+    model = Sequential()
+
+    densenet = DenseNet121(
+        weights='imagenet',
+        include_top=False,
+        input_shape=(IMG_SIZE, IMG_SIZE, 3)
+    )
+
+    model.add(densenet)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dropout(0.5))
+    # model.add(layers.Dense(NUM_CLASSES, activation='softmax'))
+    model.add(layers.Dense(NUM_CLASSES, activation='sigmoid'))
+
+    model.summary()
+
+    model.compile(loss='binary_crossentropy',
+                  # optimizer=optimizers.Adam(lr=0.0001,decay=1e-6),
+                  optimizer=optimizers.SGD(lr=0.0001, momentum=0.9),
+                  metrics=['accuracy'])
+
+    # fits the model on batches with real-time data augmentation:
+    history = model.fit_generator(
+        datagen.flow(x_train, y_train, batch_size=BATCH_SIZE, seed=1),
+        steps_per_epoch=len(x_train) // BATCH_SIZE,
+        epochs=NUM_EPOCHS,
+        validation_data=(x_test, y_test),
+        callbacks=callbacks_list,
+        max_queue_size=2
+    )
+
+
+if __name__ == '__main__':
+    main()
--- submission_check.py
+++ submission_check.py
@@ -11,7 +11,7 @@ from keras.models import load_model
 from utils import get_cur_milliseconds, get_best_preds, get_pessimist_preds
 
 
-TEST_DATA_PATH_NAME = './data/data_rgb_512.h5'
+TEST_DATA_PATH_NAME = './data/data_rgb_512_processed.h5'
 MODEL_PATH_NAME = './models/1564294047155_kappa_0.8008_val_acc_0.9356_acc_0.9299.h5'
 SUBMISSION_PATH_NAME = './submissions'
 BATCH_SIZE = 1
@@ -33,23 +33,10 @@ def main():
     file = h5py.File(TEST_DATA_PATH_NAME, 'r')
     x_test = file['x_test']
     x_test = np.asarray(x_test)
-    x_test = x_test.astype('float16')
-    # Preprocess x_test data
-    x_test = preprocess_input(x_test)
+    y_test = file['y_test']
 
     model = load_model(MODEL_PATH_NAME)
-    predictions = model.predict(x_test, batch_size=BATCH_SIZE, verbose=1)
-    # Returns boolean array of True and False [ True, False, False, ... ]
-    y_pred = predictions > 0.5
-
-    # This is the standard encoding where we sum each row
-    y_pred_sum = get_sum_preds(y_pred)
-    y_pred_best = get_best_preds(y_pred)
-    y_pred_pessimist = get_pessimist_preds(y_pred)
-
-    create_submission(y_pred_sum, milliseconds, 'sum_encoding')
-    create_submission(y_pred_best, milliseconds, 'best_encoding')
-    create_submission(y_pred_pessimist, milliseconds, 'pessimistic_encoding')
+    model.evaluate(
 
 
 if __name__ == '__main__':
